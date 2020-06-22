7299b3c86aeb074b10a2bf9268a4076b0ac1310c
felipehenrique159 <felipecod6@hotmail.com>
2020-06-22 16:44:54 -0300

    rota profile

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ const express = require('express')
 const connection = require('./database/connection')
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 
 const routes = express.Router()
 
 routes.get('/ongs',OngController.show)
 routes.get('/incidents',IncidentController.show)
+routes.get('/profile',ProfileController.index)
 
 routes.post('/ongs',OngController.create)
 routes.post('/incidents',IncidentController.create)
